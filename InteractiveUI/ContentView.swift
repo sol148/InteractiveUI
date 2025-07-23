e8b97f0a8768d5c7c2871f774d5848b366aab55b
Sol Han <solianinahan@gmail.com>
2025-07-23 15:38:51 -0500

    Initial Commit

---
+++ InteractiveUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InteractiveUI
+//
+//  Created by Sol Han on 7/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
